9709c25c5a8b88f576f9a809995059ffc17ce278
LovisCoding <arthur.lecomte@etu.univ-lehavre.fr>
2024-12-02 09:31:54 +0100

    Add Navbar component and update routing; include @tabler/icons-react dependency

--- routes/AppRoutes.jsx
+++ routes/AppRoutes.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router';
 import App from '../src/App';
 import Test from '../src/pages/Test/Test';
+import Navbar from '../src/components/Navbar';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<App />} />
 	  <Route path="/test/:id" element={<Test />} />
+	  <Route path="/home" element={<Navbar/>} />
 
     </Routes>
   );
