dc34e6c578b3ce2a94adcca9ed317a65dd6637b6
Florian Janot <janotflorian@gmail.com>
2024-12-06 16:58:00 +0100

    done

--- src/pages/ConfirmAccount.jsx
+++ src/pages/ConfirmAccount.jsx
@@ -1,13 +1,14 @@
 import { Typography } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router"
+import { useNavigate, useParams } from "react-router";
 
-export default function ConfirmAccount(){
+export default function ConfirmAccount() {
 
 	const navigate = useNavigate();
 	const { token } = useParams();
 	const [msg, setMsg] = useState("Confirmation du compte... ");
+	const [subMsg, setSubMsg] = useState("");
 
 	useEffect(() => {
 		axios.post("/api/account/confirmAccount", {
@@ -15,18 +16,24 @@ export default function ConfirmAccount(){
 		})
 		.then((res) => {
 			if (res.status == 201) {
-				navigate("/login");
-				setMsg(res.data);
+				setMsg("Votre a été activé avec succès !");
+				setTimeout(() => {
+					navigate("/login");
+				}, 3000);
 			} else {
 				setMsg(res.data);
 			}
 		})
 		.catch(() => {
-			setMsg("Une erreur est survenue")
+			setMsg("Erreur lors de l'activation du compte.");
+			setSubMsg("Le lien peut être invalide ou expiré.");
 		})
 	}, [])
 
 	return(
-		<Typography variant="h1" textAlign="center">{msg}</Typography>
+		<>
+			<Typography variant="h4" mt={5} textAlign="center" >{msg}</Typography>
+			<Typography variant="h5" mt={3} textAlign="center" fontStyle="italic" >{subMsg}</Typography>
+		</>
 	)
 }
