e30df36e5d3d792ac470aac211fbb8d4042c99fc
Florian Janot <janotflorian@gmail.com>
2024-12-06 08:34:38 +0100

    done / working

--- src/services/CommandService.js
+++ src/services/CommandService.js
@@ -2,10 +2,17 @@
 import axios from "axios"
 
 export const getCommand = async (idCommand) => {
-    const data = await axios.get('/api/client/commande/get-commande', {params:{idCommande:idCommand}});
+    const data = await axios
+        .get('/api/client/commande/get-commande', {params:{idCommande:idCommand}});
     if (!data.data) return null;
     return data.data
 }
 
 
+export const getCommandProducts = async (idCommand) => {
+    const data = await axios
+        .get('/api/client/commandeproduit/get-produits-commande', {params:{idCommande:idCommand}});
+    if (!data.data) return null;
+    return data.data;
+}
 
