d37b47f5c162592ce10c8e9a87c985efb0343933
KLS <kyliannlvs2@gmail.com>
2024-12-09 13:16:08 +0100

    Réalisation processus envoi image configuration page Accueil depuis Admin

--- src/services/HomeService.js
+++ src/services/HomeService.js
@@ -1,8 +1,16 @@
 import axios from 'axios';
 
-export const addImage= async (image, type) => {
+export const addImage = async (image, type) => {
 	const data = await axios.post('/api/admin/personnalisation/upload-image', {
 		image,
 		type
+	})
+}
+
+export const addImageCateg = async (idCateg, libImage, image) => {
+	const data = await axios.post('/api/categorie/update-image', {
+		idCateg,
+		libImage,
+		image
 	})
 }
