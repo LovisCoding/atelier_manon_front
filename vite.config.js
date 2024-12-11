0ddf2121c0626994d6072b34ecea83f537cc5069
LovisCoding <arthur.lecomte@etu.univ-lehavre.fr>
2024-12-11 16:47:58 +0100

    Update Vite proxy to use environment variable and update .env
    Replaced hardcoded proxy target with `VITE_URL` from `.env` for improved flexibility and configurability. Updated `.env` to point to the new server URL. This change enhances adaptability across environments and reduces hardcoding.

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	server: {
-		proxy: {
-			'/api': {
-				target: import.meta.env.VITE_URL,
-				changeOrigin: true,
+export default defineConfig(({mode}) => {
+	const env = loadEnv(mode, process.cwd());
+	return (
+		{
+			plugins: [react()],
+			server: {
+				proxy: {
+					'/api': {
+						target: env.VITE_URL,
+						changeOrigin: true,
+					},
+				},
 			},
-		},
-	},
-});
+
+	});
+})
