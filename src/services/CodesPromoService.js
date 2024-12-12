3383f758c4cd92cc6430c2f2d6330ebe717e5936
Matthias Bernouy <matt.bernouy@orange.fr>
2024-12-12 23:00:47 +0100

    .

--- src/services/CodesPromoService.js
+++ src/services/CodesPromoService.js
@@ -4,7 +4,7 @@ import api from "../utils/api";
 
 export const getAllCodesPromo = async () => {
     const data = await api
-        .get('/api/codepromo/get-codespromo')
+        .get('/api/admin/codepromo/get-codespromo')
     if (data.data) return data.data
     return null;
 }
@@ -26,7 +26,7 @@ export const getOneCodePromo = async (code) => {
 }
 
 export const CreatePromo = async (obj) => {
-    return api.post('/api/codepromo/add-codepromo', {
+    return api.post('/api/admin/codepromo/add-codepromo', {
         ...obj
     })
     .then((res) => {
@@ -40,7 +40,7 @@ export const CreatePromo = async (obj) => {
 
 export const DeleteCodePromo = async (code) => {
     const data = await api
-        .delete('/api/codepromo/delete-codepromo', {
+        .delete('/api/admin/codepromo/delete-codepromo', {
             data: {
                 code
             }
